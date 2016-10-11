9bca69146882eb34b0d71dbf277b05957b74520e
iangthomas <Ian@Ians-MacBook-Pro.local>
2016-10-11 00:22:30 -0700

    Added the header from the 2009 original iAssign Xcode project.

--- iAssign/Assignment.swift
+++ iAssign/Assignment.swift
@@ -5,6 +5,20 @@
 //  Created by Ian Thomas on 10/10/16.
 //  Copyright © 2016 KKIT Creations. All rights reserved.
 //
+/*
+ SQLite Book List
+ 
+ 
+ This sample application is primarily focused on using the SQLite database with an iPhone application, and secondarily with leveraging that database to respond to low memory warnings. In addition, the sample demonstrates one approach to implementing a master-detail interface
+ 
+ Build Requirements
+ Mac OS X 10.5.3, Xcode 3.1, iPhone OS 2.0.
+ 
+ Runtime Requirements
+ Mac OS X 10.5.3, iPhone OS 2.0.
+ 
+ Circa June 2009
+ */
 
 import UIKit
 
